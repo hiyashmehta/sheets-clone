2d37dd3f5ec0ba7f1c03a25f0dd1c96ad0d99da9
Yash Mehta <yashmehta0395@gmail.com>
2023-09-15 17:23:27 +0530

    added cells to the grid

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let cols = 26;
 
 let addressColCont = document.querySelector(".address-col-cont");
 let addressRowCont = document.querySelector(".address-row-cont");
+let cellsCont = document.querySelector(".cells-cont");
 
 for (let i = 0;i < rows;i++) {
     let addressCol = document.createElement("div");
@@ -14,6 +15,17 @@ for (let i = 0;i < rows;i++) {
 for (let i = 0;i < cols;i++) {
     let addressRow = document.createElement("div");
     addressRow.setAttribute("class", "address-row");
-    addressRow.innerText = i+1;
+    addressRow.innerText = String.fromCharCode(65 + i);
     addressRowCont.appendChild(addressRow);
+}
+
+for (let i = 0;i < rows;i++) {
+    let rowCont = document.createElement("div");
+    rowCont.setAttribute("class", "row-cont");
+    for (let j = 0;j < cols;j++) {
+        let cell = document.createElement("div");
+        cell.setAttribute("class", "cell");
+        rowCont.appendChild(cell);
+    }
+    cellsCont.appendChild(rowCont);
 }
