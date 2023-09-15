d930fbbcb29b8b54332fc8e51662f2dff7355149
Yash Mehta <yashmehta0395@gmail.com>
2023-09-15 16:45:37 +0530

    added row and cols address for the grid

---
+++ script.js
@@ -0,0 +1,19 @@
+let rows = 100;
+let cols = 26;
+
+let addressColCont = document.querySelector(".address-col-cont");
+let addressRowCont = document.querySelector(".address-row-cont");
+
+for (let i = 0;i < rows;i++) {
+    let addressCol = document.createElement("div");
+    addressCol.setAttribute("class", "address-col");
+    addressCol.innerText = i+1;
+    addressColCont.appendChild(addressCol);
+}
+
+for (let i = 0;i < cols;i++) {
+    let addressRow = document.createElement("div");
+    addressRow.setAttribute("class", "address-row");
+    addressRow.innerText = i+1;
+    addressRowCont.appendChild(addressRow);
+}
